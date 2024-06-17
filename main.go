8d6e3847d352d717af4c423c917e2456032ee127
hacdan <david.mcclellan.p@gmail.com>
2024-06-17 13:16:17 -0400

    Change to slog

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -15,12 +16,13 @@ const (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(err)
+		slog.Error("Error loading environment variables", err)
 	}
 
 	http.HandleFunc("/api/v1/codes", handleAllCodes)
 	http.HandleFunc("/api/v1/codes/{code}", handleCode)
 	http.HandleFunc("/api/v1/codes/parse", handleParse)
 
-	log.Fatal(http.ListenAndServe(getEnv("ADDRESS", DefaultListenPort), nil))
+	slog.Error("Server failed: ", http.ListenAndServe(getEnv("ADDRESS", DefaultListenPort), nil))
+	os.Exit(1)
 }
